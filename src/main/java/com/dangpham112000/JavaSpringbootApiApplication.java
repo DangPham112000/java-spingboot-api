d0ead49844cea2ec4de32aac728436513ab49646
DangPham112000 <dangpham112000@gmail.com>
2024-10-14 17:51:01 +0700

    init project

---
+++ src/main/java/com/dangpham112000/JavaSpringbootApiApplication.java
@@ -0,0 +1,13 @@
+package com.dangpham112000;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringbootApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringbootApiApplication.class, args);
+	}
+
+}
